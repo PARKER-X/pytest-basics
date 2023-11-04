1f8317e09f05ca216ca753165aaa5f0ac04652f6
Harsh <harshrawat096@gmail.com>
2023-11-04 13:00:01 +0530

    My_function is created!

---
+++ source/my_function.py
@@ -0,0 +1,11 @@
+def add_number(number1,number2):
+    return number1+number2
+
+def sub_number(number1,number2):
+    return number1-number2
+
+def mul_number(number1,number2):
+    return number1*number2
+
+def divide_number(number1,number2):
+    return number1/number2
